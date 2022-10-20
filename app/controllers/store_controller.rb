34dcb0b0c08c883358afa27cedd716622eeccb51
Mark Prima Nabil Akbar <prime.mark.22@gmail.com>
2022-10-21 06:48:02 +0700

    make it better

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,5 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
-    # command `.order`to invoke all products alphabetically from title
   end 
 end

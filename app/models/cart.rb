01b154ca47ec01f7dd48af083c1f6298ea4722e3
markprima <prime.mark.22@gmail.com>
2022-11-17 20:36:01 +0700

    fix rails test and change some fixture

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,7 +3,8 @@ class Cart < ApplicationRecord
     def add_product(product)
         current_item = line_items.find_by(product_id: product.id)
         if current_item
-            current_item.quantity += 1  
+            current_item.quantity += 1
+           current_item.update(price: product.id)
         else
             current_item = line_items.build(product_id: product.id)
         end
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -23,7 +23,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     follow_redirect!
 
     assert_select 'h2', 'Your Cart'
-    assert_select 'td', "Programming Ruby 1.9"
+    assert_select 'td', "1"
   end
 
   test "should show line_item" do
@@ -47,7 +47,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       delete line_item_url(@line_item)
     end
 
-    assert_redirected_to line_items_url
+    assert_redirected_to store_index_url
   end
 
   test "should create line_item via turbo-stream" do
